996ec99ee3d775c04562ea3105ce0683a86fe282
Oon Chee Meng <34565690+Cheemeng97@users.noreply.github.com>
2023-06-27 19:59:40 +0800

    requirement.txt

--- EncyclopediaCrawler/EncyclopediaCrawler/spiders/epCategoriesSpider.py
+++ EncyclopediaCrawler/EncyclopediaCrawler/spiders/epCategoriesSpider.py
@@ -1,11 +1,6 @@
 from pathlib import Path
 
 import scrapy
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 encyclopediaDefaultUrl = "https://www.encyclopedia.com"
 
